2df9d10d9fd270454567f5c17e74552b3d9bbf03
mirabellensaft <tanja@transfeld.net>
2017-10-31 14:13:53 +0100

    added user authentication forms are mostly not working

--- website/models.py
+++ website/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -12,6 +13,26 @@ class Post(models.Model):
     text = models.TextField('Description')
     image = models.ImageField(upload_to='img', height_field=None, width_field=None)
     number = models.IntegerField(default=0)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank = True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Contact(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField('Contact', max_length=200)
+    location = models.CharField('Description', max_length=200)
+    phone = models.CharField('Description', max_length=200)
+    github = models.URLField('Description')
+    codepen = models.URLField('Description')
+    twitter = models.URLField('Description')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
